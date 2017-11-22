2368fe8bbef6c3213775bbd58ad9dda0b9f981af
nikita ahuja <nahuja038@gmail.com>
2017-11-22 15:25:10 -0600

    Init commit

---
+++ application.js
@@ -0,0 +1,36 @@
+$(document).ready(function(){
+  var testNumLength = function(number) {
+        if (number.length > 9) {
+            totaldiv.text(number.substr(number.length-9,9));
+            if (number.length > 15) {
+                number = "";
+                totaldiv.text("Err");
+            }
+        }
+    };
+  var number = "";
+    var newnumber = "";
+    var operator = "";
+    var totaldiv = $("#total");
+    totaldiv.text("0");
+
+$("#numbers > a").not("#clear,#clearall").click(function(){
+    // console.log("clicked");
+    number += $(this).html();
+    totaldiv.text(number);
+    testNumLength(number);
+});
+
+$("#clear").on("click", function(){
+    console.log("clear");
+    $("#total").text("");
+})
+
+$("#clearall").on("click", function(){
+    console.log("clear");
+    $("#total").text("");
+})
+
+
+
+});
